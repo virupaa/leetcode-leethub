2df9a2e788faaab761d45cc7c03ad1cabd2396df
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-08-21 21:19:30 -0500

    Time: 34 ms (61.42%), Space: 16.5 MB (14.64%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        bin_num = bin(num)
+        a = list(bin_num[2:])
+        print(a)
+        b = []
+        for i in a:
+            if i == '0':
+                i = '1'
+                b.append(i)
+            else:
+                i = '0'
+                b.append(i)
+        return int(''.join(b),2)
+        
