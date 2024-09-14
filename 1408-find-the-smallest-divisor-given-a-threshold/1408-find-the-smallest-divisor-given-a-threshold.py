0dda10c547f7b17003daaecfd14984973deeb4c2
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-09-14 17:31:47 -0400

    Time: 282 ms (32.14%), Space: 22.8 MB (55.74%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.py
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.py
@@ -2,7 +2,7 @@ class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         def feasible(number):
             for num in nums:
-                return sum((num -1 ) // number + 1 for num in nums) <= threshold
+                return sum(math.ceil(num / number) for num in nums) <= threshold
 
 
         left, right = 1, max(nums)
