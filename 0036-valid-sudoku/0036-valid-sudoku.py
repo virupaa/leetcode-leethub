db5b9fb932f4dc17d22d113790128d18a103daeb
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-09-19 08:36:26 -0400

    Time: 89 ms (74.91%), Space: 16.5 MB (51.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,41 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            s = set()
-            for j in range(9):
-                item = board[i][j]
-                if item in s:
-                    return False
-                elif item != '.':
-                    s.add(item)
-        
-        for i in range(9):
-            s = set()
-            for j in range(9):
-                item = board[j][i]
-                if item in s:
-                    return False
-                elif item != '.':
-                    s.add(item)
-        
-        starts = [(0,0),(0,3), (0,6),
-                   (3,0),(3,3), (3,6),
-                   (6,0),(6,3), (6,6)]
-        
-        
-        for i, j in starts:
-            s = set()
-            for row in range(i,i+3):
-                for col in range(j,j+3):
-                    item = board[row][col]
-                    if item in s:
-                        return False
-                    elif item != '.':
-                        s.add(item)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                   board[r][c] in cols[c] or
+                   board[r][c] in squares[(r//3,c//3)]):
+                   return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
         return True
-
-        
-
-                
-
-        
