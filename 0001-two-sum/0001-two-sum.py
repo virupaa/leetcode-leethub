50ee0c3e5a13dc54bdd1e9a28c228614148fad6a
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-10-15 19:45:28 -0400

    Time: 62 ms (51.57%), Space: 17.8 MB (39.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Dictionary to store the value and its index
         seen = {}
-        
-        # Loop through the list
         for i, num in enumerate(nums):
-            # Calculate the complement that would sum with num to get the target
             complement = target - num
-            
-            # If the complement is already in the dictionary, return its index and the current index
+
             if complement in seen:
                 return [seen[complement], i]
             
-            # Otherwise, add the current number and its index to the dictionary
             seen[num] = i
+        
