60057b5d9929240773d0483f25591fe079a68257
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-10-14 20:21:51 -0400

    Time: 50 ms (86.38%), Space: 16.8 MB (34.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,25 +1,44 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1 = len(s1)
-        n2 = len(s2)
-        count_s1 = [0] * 26
-        count_s2 = [0] * 26
-        if n1 > n2:
-            return False
-        
-        for i in range(n1):
-            count_s1[ord(s1[i]) - 97] += 1
-            count_s2[ord(s2[i]) - 97] += 1
-        
-        if count_s1 == count_s2:
-            return True
-        
-        for i in range(n1,n2):
-            count_s2[ord(s2[i]) - 97] += 1
-            count_s2[ord(s2[i-n1]) - ord('a')] -= 1
-            if count_s1 == count_s2:
-                return True 
-        return False
+        if len(s1) > len(s2): return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+            else:
+                matches += 0
+        l = 0
+        for r in range(len(s1),len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
 
 
+            
+
+
+
+        
         
