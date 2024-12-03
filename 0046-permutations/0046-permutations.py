1781f427bedb06ba2a9401345a40fd5d652685a1
Chinmay <43137090+virupaa@users.noreply.github.com>
2024-12-02 20:04:17 -0600

    Time: 0 ms (100%), Space: 17.6 MB (6.62%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        perms = self.permute(nums[1:])
         res = []
-        if len(nums) == 1:
-            return [nums[:]]
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-        
-            for perm in perms:
-                perm.append(n)
-            res.extend(perms)
-            nums.append(n)
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
         return res
-
-
-
-        
-        
